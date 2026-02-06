b4d597a6a2cb12a82c5a78a007663a8beac8aabe
Andrei Chirica <andrei.chirica@emag.ro>
2026-02-06 15:45:08 +0200

    Add Google Analytics tracking to landing page
    - Integrated Google Analytics 4 script into the landing page to enable tracking of user interactions.
    - The script is conditionally included based on the presence of the GOOGLE_ANALYTICS_ID in the environment configuration.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -29,6 +29,18 @@
     <link rel="shortcut icon" href="{{ asset('favicon-32x32.png') }}?v=3">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon-96x96.png') }}?v=3">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    @if(env('GOOGLE_ANALYTICS_ID'))
+    <!-- Google Analytics 4 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '{{ env('GOOGLE_ANALYTICS_ID') }}');
+    </script>
+    @endif
+    
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
